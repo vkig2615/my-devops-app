d8769772b07e6d26e08f4e45e6b158d9e832526f
vinoth <vinothvkvk384@gmail.com>
2025-10-02 15:56:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Welcome to DevOps World 🚀</h1>
+      <p>Powered by React & Docker</p>
+      <img 
+        src="https://www.docker.com/wp-content/uploads/2022/03/Moby-logo.png" 
+        alt="DevOps Logo" 
+        width="300"
+      />
+    </div>
+  );
+}
+
+export default App;
+
